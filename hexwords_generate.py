dbce6e55d1166687418669c1456589a4b5b04f4e
Warrior <youaredev123@gmail.com>
2022-05-25 21:53:51 +0300

    added validation of  random hex's repetition

--- hexwords_generate.py
+++ hexwords_generate.py
@@ -12,20 +12,21 @@ def main():
     ]
 
     reHexWord = re.compile("[a-f0-9]*")
+
     ## open and read english dictionary text file
-    fWords = open('words.txt', 'r')
-    ## create hexwords list file
-    fHexwords = open('hexspeakwords.txt', 'a')
-    for w in fWords.readlines():
-        w = w.strip()
-        for old, new in subs:
-            w = w.replace(old, new)
-        if len(w) >= minlen:
-            match = reHexWord.search(w)
-            if match and match.group() == w:
-                ## write hexwords to hexspeakwords.txt file
-                fHexwords.write(w + '\n')
-    fHexwords.close()
+    with open('words.txt', 'r') as fWords:
+        
+        ## create hexwords list file
+        with open('hexspeakwords.txt', 'a') as fHexwords:
+            for w in fWords.readlines():
+                w = w.strip()
+                for old, new in subs:
+                    w = w.replace(old, new)
+                if len(w) >= minlen:
+                    match = reHexWord.search(w)
+                    if match and match.group() == w:
+                        ## write hexwords to hexspeakwords.txt file
+                        fHexwords.write(w + '\n')
     print("########### hexspeak words list created successfully. ###########")
 if __name__ == '__main__':
     main()
--- run.py
+++ run.py
@@ -2,26 +2,27 @@ from asyncio.windows_events import NULL
 import secrets
 def main():
     while True:
+
         ## Generates 8-digit hexadecimal code
         randomHex = secrets.token_hex(4)
-        ## odd-looking hex code list
-        odd_looking_words = ['aa', 'bb', 'cc', 'dd', 'ee', 'ff', '01', '12', '23', '34', '45', '56', '67', '78', '89', '90']
-        ## open and read hexwords
-        fHexwords = open('hexspeakwords.txt', 'r')
+
+        ## odd-looking hex code list. we can update these odd-looking list
+        odd_looking_words = ['aa', 'bb', 'cc', 'dd', 'ee', 'ff', 'ab', 'bc', 'cd', 'de', 'ef', '00', '11', '22', '33', '44', '55', '66', '77', '88', '99', '01', '12', '23', '34', '45', '56', '67', '78', '89']
+
         ## validate random hex code if it contains any odd-looking hex code
-        for odd in odd_looking_words:
-            if odd in randomHex:
-                randomHex = NULL
-                break
-        if randomHex == NULL:
+        if any(odd in randomHex for odd in odd_looking_words):
             continue
-        ## validate random hex code if it contains any hexword(hexspeak words)
-        for hexword in fHexwords.readlines():
-            if hexword.strip() in randomHex:
-                randomHex = NULL
-                break
-        if randomHex == NULL:
-            continue
+
+        ## open and read hexwords
+        with open('hexspeakwords.txt', 'r') as fHexwords:
+            ## validate random hex code if it contains any hexword(hexspeak words)
+            if any(hexword in randomHex for hexword in fHexwords.readlines()):
+                continue
+        with open('oldHexs.txt', 'a+') as foldHexs:
+            ## validate random hex code's repetition
+            if any(randomHex in oldHex for oldHex in foldHexs.readlines()):
+                continue
+            foldHexs.write(randomHex + '\n')
         print('########### This is 8-digit hexadecimal code: 0x' + randomHex + ' ###########')
         break
 
